85715f3da5ccd29f3b8783e010d881b3489e282f
VikSem17 <vic185806@gmail.com>
2017-07-05 12:26:11 +0400

    Научиться запускать RethinkDB в Docker

---
+++ StartRethinkDB.bat
@@ -0,0 +1,9 @@
+docker-machine env --shell cmd default
+SET DOCKER_TLS_VERIFY=1
+SET DOCKER_HOST=tcp://192.168.99.100:2376
+SET DOCKER_CERT_PATH=C:\Users\DELL\.docker\machine\machines\default
+SET DOCKER_MACHINE_NAME=default
+SET COMPOSE_CONVERT_WINDOWS_PATHS=true
+REM Run this command to configure your shell:
+REM     @FOR /f "tokens=*" %i IN ('docker-machine env --shell cmd default') DO @%i
+docker run --name rethink -p "8080:8080" -p "28015:28015" -d rethinkdb
